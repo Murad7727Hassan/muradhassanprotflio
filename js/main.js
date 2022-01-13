13e2a01038389c75f95b88a305c9c7538367afee
murad7727hassan <murad7727hassan@gmail.com>
2022-01-13 16:05:16 +0300

    add features and js file

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// Get the root element
+var r = document.querySelector(':root');
+
+// Create a function for getting a variable value
+function myFunction_get() {
+    // Get the styles (properties and values) for the root
+    var rs = getComputedStyle(r);
+    // Alert the value of the --blue variable
+     r.style.setProperty('--font-header-color', 'black');
+     r.style.setProperty('--secondary-color', 'white');
+     r.style.setProperty(' --font-content-color', 'black');
+  }
+  
+  function myFunction_set() {
+    // Get the styles (properties and values) for the root
+    var rs = getComputedStyle(r);
+    // Alert the value of the --blue variable
+     r.style.setProperty('black','#CCCCCC');
+     r.style.setProperty( 'white','#303131');
+     r.style.setProperty('black',' #808899');
+  }
+  
+    //   check namme 
+    document.getElementById('myBtn').addEventListener('click', function(e){ //say this is an anchor
+        //do something
+       e.preventDefault();
+       let name = document.getElementById("name");
+           email = document.getElementById("email");
+       if(name.length  > 3 && name.length < 100 )        
+            {
+                name.style.color = "green";
+                name.style.border = " 1px solid green"; 
+            }
+            else
+            {
+                name.style.color = "red";
+                name.style.border = "1px solid red";;  
+            }
+            //  check email valid 
+            let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            if (email.match(regexEmail))
+            {
+                email.style.color = "green";
+                email.style.border = " 1px solid green"; 
+            }
+            else
+            {
+                email.style.color = "red";
+                email.style.border = "1px solid red";;  
+            }
+      
+  });
+  function view()
+  {
+    document.getElementById("im").style.position = "absolute" ;
+    document.getElementById("im").style.top = "25%" ;
+    document.getElementById("im").style.left = "25%" ;
+    document.getElementById("im").style.zIndex = " " ;
+  }
